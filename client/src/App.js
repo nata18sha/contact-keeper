a21da0a8189109f6c085737a5be6e4ca9767599a
nata18sha <nata18sha@gmail.com>
2021-08-13 17:35:49 +0300

    global state  and contact context created

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,30 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
+import ContactState from './context/contact/ContactState';
 import './App.css';
 
 const App = () => {
-    return (
-        <Router>
-            <Fragment>
-                <Navbar />
-                <div className="container">
-                    <Switch>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
-                        <Route exact path="/about">
-                            <About />
-                        </Route>
-                    </Switch>
-                </div>
-            </Fragment>
-        </Router>
-    );
+	return (
+		<ContactState>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<div className="container">
+						<Switch>
+							<Route exact path="/">
+								<Home />
+							</Route>
+							<Route exact path="/about">
+								<About />
+							</Route>
+						</Switch>
+					</div>
+				</Fragment>
+			</Router>
+		</ContactState>
+	);
 };
 
 export default App;
